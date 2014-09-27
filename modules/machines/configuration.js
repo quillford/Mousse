55f2895d7ba046519ab272fc3e8caa931a4055a3
Arthur Wolf <wolf.arthur@gmail.com>
2014-09-27 22:35:36 +0200

    added configuration parsing, the configuration object now caries configuration on itself

--- modules/machines/configuration.js
+++ modules/machines/configuration.js
@@ -9,9 +9,52 @@ var Configuration = new Class({
         // Retrieve the configuration from the machine, parse it and store it
         this.machine.interface.get_file({
             file: "/sd/config", 
-            done: function( result ){ console.log(result); },
+            done: this.parse_config_file,
             caller: this    
         });
+    },
+
+    parse_config_file: function( config_file ){
+        // Parse the config file and store the values in myself 
+        config_file = config_file.split("\n");
+    
+        // For each line in the config file
+        for(var index in config_file){
+            var line = config_file[index];
+    
+            // Skip obviously un-interresting lines
+            if( line.substr(0,1) == '#' || line.substr(0,1) == ' ' || !line ){ continue; }
+
+            // Remove comments at the end of lines
+            line = line.split('#')[0];
+
+            // Extract key and value
+            var matches = line.match(/^\s*([^\s]+)\s*([^\s]+)\s*$/);
+
+            // If no matches
+            if( !matches ){ continue; }
+
+            // We have a match
+            var key = matches[1];
+            var value = matches[2];
+
+            // Extract key path
+            var key_path = key.split('.');
+
+            // Assign values to ourselves depending on length of they path
+            if( key_path.length == 1 ){
+                this[key] = value;
+            }else if(key_path.length == 2 ){
+                if( this[key_path[0]] == undefined ){ this[key_path[0]] = {}; } 
+                this[key_path[0]][key_path[1]] = value;
+            }else if(key_path.length == 3 ){
+                if( this[key_path[0]] == undefined ){ this[key_path[0]] = {}; } 
+                if( this[key_path[0]][key_path[1]] == undefined ){ this[key_path[0]][key_path[1]] = {}; }
+                this[key_path[0]][key_path[1]][key_path[2]] = value;
+            } 
+        
+        } 
+
     }
 
 
