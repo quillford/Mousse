c642c1a80e79905b11279f98cbf18eb498a1af46
quillford <quillford@users.noreply.github.com>
2015-04-20 20:50:00 -0700

    Cleaned up controlscreen module.js

--- modules/gui/screens/controlscreen/module.js
+++ modules/gui/screens/controlscreen/module.js
@@ -123,61 +123,7 @@ var Controlscreen = Module.extend({
                 };
               }
         });
-    },
-    
-    // Send gcode to the machine
-    send_gcode: function( command ){
-        // Get the machine's IP address
-        this.ip = this.selected_machine.ip;
-
-        // Add a new line to the command
-        this.command = command.command + "\n";
-
-        console.log("sending: "+this.command);
-        // Send the command
-        if(command.send_response){
-            $.ajax({ 
-                url: "http://" + this.ip + '/command',
-                caller: this,
-                type: "POST",
-                data: this.command,
-                async: true,
-            }).done(function(result){console.log("gcode successfully sent."); kernel.call_event("update_console", result);}).fail(function(){console.log("Failed to send gcode.");});
-        }else {
-            $.ajax({ 
-                url: "http://" + this.ip + '/command',
-                caller: this,
-                type: "POST",
-                data: this.command,
-                async: true,
-            }).done(function(result){console.log("gcode successfully sent.");}).fail(function(){console.log("Failed to send gcode.");});
-        }
-    },
-    
-    // Send gcode without displaying a response
-    send_gcode_silent: function( command ){
-        this.send_gcode( {command: command, send_response: false} );
-    },
-    
-    // Display the machine's response to the sent command
-    update_console: function( response ){
-        $("#command_response").text( response );
-    },
-    
-    // Update values such as temperature and progress
-    on_value_update: function(result){
-        // Set the Temperature
-        $("#tempReport").text(result.temperature.T.temperature+"°C");
-        
-        // Set print_progress
-        if (result.progress.playing){
-            $("#print_progress").text(result.progress.string);
-        }else {
-            $("#print_progress").text("No print in progress");
-        }
-        
     }
-
 });
 
 
